dcd6f1998a066825475ddfc0cc66416a7075e6e6
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-02-21 17:04:52 +0100

    improvement design Users Navbar

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -1,3 +1,6 @@
+import React, { useMemo } from 'react';
+
+
 import { generateRandomName } from "@/lib/utils";
 import { useOthers, useSelf } from "@/liveblocks.config";
 import { Avatar } from "./Avatar";
@@ -8,16 +11,17 @@ const ActiveUsers = () => {
     const users = useOthers();
     const currentUser = useSelf();
     const hasMoreUsers = users.length > 3;
-  
-    return (
-      <main className="flex h-screen w-full select-none place-content-center place-items-center">
+
+    const memoizedUsers = useMemo(() => {
+      return (
+        <div className="flex items-center justify-center gap-1 py-2">
         <div className="flex pl-3">
 
         {currentUser && (
               <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
           )}
 
-          {users.slice(0, 3).map(({ connectionId, info }) => {
+          {users.slice(0, 3).map(({ connectionId }) => {
             return (
               <Avatar key={connectionId} name={generateRandomName()} otherStyles="-ml-3" />
             );
@@ -26,8 +30,11 @@ const ActiveUsers = () => {
           {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
   
         </div>
-      </main>
-    );
+      </div>
+      )
+    }, [users.length])
+  
+    return memoizedUsers;
   }
 
   export default ActiveUsers;
