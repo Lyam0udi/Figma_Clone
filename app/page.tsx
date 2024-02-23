ee8e4165947788b7e8805675755a0fc2b56d3cc4
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-02-23 16:53:40 +0100

    Toolbar and ShapesMenu

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -15,6 +16,19 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<String | null>('rectangle');
+
+  const [activeElement, setactiveElement] = useState<ActiveElement>({
+    name: '',
+    value: '',
+    icon: '',
+
+  })
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setactiveElement(elem);
+
+    selectedShapeRef.current = elem?.value as String;
+  }
 
   useEffect(() => {
     const canvas = initializeFabric({ canvasRef, fabricRef })
@@ -35,7 +49,10 @@ export default function Page() {
 
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
       <section className="flex h-full flex-row">
         <LeftSidebar />
         <Live canvasRef={canvasRef}/>
