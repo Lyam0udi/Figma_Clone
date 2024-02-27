3638ea5a0cb6068ce53702b3d14e884e3c88fbd1
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-02-27 19:35:53 +0100

    LastUpdates

--- app/Room.tsx
+++ app/Room.tsx
@@ -1,23 +1,34 @@
 "use client";
 
-import { ReactNode } from "react";
-import { RoomProvider } from "../liveblocks.config";
-import { ClientSideSuspense } from "@liveblocks/react";
 import { LiveMap } from "@liveblocks/client";
+import { ClientSideSuspense } from "@liveblocks/react";
+
 import Loader from "@/components/Loader";
+import { RoomProvider } from "@/liveblocks.config";
 
-
-
-export function Room({ children }: { children: ReactNode }) {
+const Room = ({ children }: { children: React.ReactNode }) => {
   return (
-    <RoomProvider id="my-room" 
-      initialPresence={{
-        cursor: null,
-        cursorColor: null,
-        editingText: null
-      }}
+    <RoomProvider
+      id="fig-room"
+      /**
+       * initialPresence is used to initialize the presence of the current
+       * user in the room.
+       *
+       * initialPresence: https://liveblocks.io/docs/api-reference/liveblocks-react#RoomProvider
+       */
+      initialPresence={{ cursor: null, cursorColor: null, editingText: null }}
+      /**
+       * initialStorage is used to initialize the storage of the room.
+       *
+       * initialStorage: https://liveblocks.io/docs/api-reference/liveblocks-react#RoomProvider
+       */
       initialStorage={{
-        canvasObjects: new LiveMap()
+        /**
+         * We're using a LiveMap to store the canvas objects
+         *
+         * LiveMap: https://liveblocks.io/docs/api-reference/liveblocks-client#LiveMap
+         */
+        canvasObjects: new LiveMap(),
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>
@@ -26,3 +37,5 @@ export function Room({ children }: { children: ReactNode }) {
     </RoomProvider>
   );
 }
+
+export default Room;
--- components/cursor/LiveCursors.tsx
+++ components/cursor/LiveCursors.tsx
@@ -1,26 +1,24 @@
-import React from 'react'; // Import React if not already imported
-import Cursor from './Cursor';
-import { LiveCursorProps } from '@/types/type';
-import { COLORS } from '@/constants/index';
+import Cursor from "./Cursor";
+import { COLORS } from "@/constants";
+import { LiveCursorProps } from "@/types/type";
 
-const LiveCursors: React.FC<LiveCursorProps> = ({ others }) => {
-  return (
-    <>
-      {others.map(({ connectionId, presence }) => {
-        if (!presence?.cursor) return null;
+// display all other live cursors
+const LiveCursors = ({ others }: LiveCursorProps) => {
+  return others.map(({ connectionId, presence }) => {
+    if (presence == null || !presence?.cursor) {
+      return null;
+    }
 
-        return (
-          <Cursor
-            key={connectionId}
-            color={COLORS[Number(connectionId) % COLORS.length]}
-            x={presence.cursor.x}
-            y={presence.cursor.y}
-            message={presence.message}
-          />
-        );
-      })}
-    </>
-  );
+    return (
+      <Cursor
+        key={connectionId}
+        color={COLORS[Number(connectionId) % COLORS.length]}
+        x={presence.cursor.x}
+        y={presence.cursor.y}
+        message={presence.message}
+      />
+    );
+  });
 };
 
 export default LiveCursors;
